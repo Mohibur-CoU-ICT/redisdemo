99ff2730487ce73255669123ee930e657a07f270
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-08-29 00:21:59 +0600

    remove redis cache related code

--- src/main/java/com/mohibur/redisdemo/RedisDemoApplication.java
+++ src/main/java/com/mohibur/redisdemo/RedisDemoApplication.java
@@ -2,10 +2,8 @@ package com.mohibur.redisdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class RedisDemoApplication {
 
     public static void main(String[] args) {
